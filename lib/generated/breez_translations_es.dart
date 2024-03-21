427c2e631cbc1066a62ddc17cf1773bf8e2f7d60
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-03-21 12:51:56 +0300

    Remove "This address can be used only once" from ln address fee texts

--- lib/generated/breez_translations_es.dart
+++ lib/generated/breez_translations_es.dart
@@ -3668,27 +3668,27 @@ class BreezTranslationsEs extends BreezTranslations {
 
   @override
   String invoice_ln_address_channel_not_needed(String minSats, String maxSats) {
-    return 'Send more than $minSats and up to $maxSats to this address.  This address can be used only once.';
+    return 'Send more than $minSats and up to $maxSats to this address.';
   }
 
   @override
   String invoice_ln_address_warning_with_min_fee_account_connected(String minSats, String maxSats, String setUpFee, String minFee, String liquidity) {
-    return 'Send more than $minSats and up to $maxSats to this address. A setup fee of $setUpFee% with a minimum of $minFee will be applied for sending more than $liquidity. This address can be used only once.';
+    return 'Send more than $minSats and up to $maxSats to this address. A setup fee of $setUpFee% with a minimum of $minFee will be applied for sending more than $liquidity.';
   }
 
   @override
   String invoice_ln_address_warning_without_min_fee_account_connected(String minSats, String maxSats, String setUpFee, String liquidity) {
-    return 'Send more than $minSats and up to $maxSats to this address. A setup fee of $setUpFee% will be applied for sending more than $liquidity. This address can be used only once.';
+    return 'Send more than $minSats and up to $maxSats to this address. A setup fee of $setUpFee% will be applied for sending more than $liquidity.';
   }
 
   @override
   String invoice_ln_address_warning_with_min_fee_account_not_connected(String minSats, String maxSats, String setUpFee, String minFee) {
-    return 'Send more than $minSats and up to $maxSats to this address. A setup fee of $setUpFee% with a minimum of $minFee will be applied on the received amount. This address can be used only once.';
+    return 'Send more than $minSats and up to $maxSats to this address. A setup fee of $setUpFee% with a minimum of $minFee will be applied on the received amount.';
   }
 
   @override
   String invoice_ln_address_warning_without_min_fee_account_not_connected(String minSats, String maxSats, String setUpFee) {
-    return 'Send more than $minSats and up to $maxSats to this address. A setup fee of $setUpFee% will be applied on the received amount. This address can be used only once.';
+    return 'Send more than $minSats and up to $maxSats to this address. A setup fee of $setUpFee% will be applied on the received amount.';
   }
 
   @override
